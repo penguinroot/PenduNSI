3a850ad0f3dbbb3b557f04b32c5a0006cf122df3
Pi-Aime <113472953+Pi-Aime@users.noreply.github.com>
2025-01-05 19:07:14 +0100

    Add files via upload

--- connexion db.py
+++ connexion db.py
@@ -1,19 +1,114 @@
 # Module Imports
 import mariadb
-import sys
 import csv
 import unidecode
-
-
-
+import sys
 
 def mettre_lettre():
-    curseur.execute("DELETE FROM LETTRES")
+    """Initialise la table LETTRES avec toutes les lettres de l'alphabet."""
+    query = "DELETE FROM LETTRES"
+    curseur.execute(query)
     for i in range(26):
-        curseur.execute("INSERT INTO LETTRES VALUES (?, ?, ?)", (chr(65 + i), 0, 0))
-        conn.commit()
+        lettre = chr(65 + i)
+        query = "INSERT INTO LETTRES VALUES ('" + lettre + "', 0, 0)"
+        curseur.execute(query)
+    conn.commit()
+
+def remplir_mots(code):
+    """ Remplir la table MOTS avec les mots d'une langue spécifique. """
+    code = code[0]  # Extract the string from the tuple
+    fichier = "liste_pendu_" + code + ".csv"
+    query = "DELETE FROM MOTS WHERE langue = '" + code + "'"
+    curseur.execute(query)
+    conn.commit()
+    sys.exit("Arrêt du script avec un message")
+    with open(fichier, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=';')
+        for i, row2 in enumerate(reader, start=1):
+            row = unidecode.unidecode(''.join(row2).upper().strip())
+            print(i, row)
+
+            if "'" not in row:
+                # Vérification des doublons avant insertion
+                query_check = "SELECT 1 FROM MOTS WHERE mot = %s AND langue = %s"
+                curseur.execute(query_check, (row, langue))
+                if curseur.fetchone():
+                    print(f"Le mot '{row}' existe déjà dans la langue '{langue}', il ne sera pas inséré.")
+                else:
+                    query = "INSERT INTO MOTS (mot, difficulte, langue) VALUES (%s, 0, %s)"
+                    curseur.execute(query, (row, langue))
+
+    conn.commit()
 
 
+def calcul_proba_lettres(langue):
+    """
+    Calcule les probabilités des lettres pour une langue donnée
+    et met à jour la table LETTRES.
+    """
+    query = "DELETE FROM LETTRES"
+    curseur.execute(query)
+    conn.commit()
+    mettre_lettre()
+
+    wtotal = 0
+    query = "SELECT mot FROM MOTS WHERE langue = '" + langue + "'"
+    curseur.execute(query)
+    mots = curseur.fetchall()
+
+    for mot_tuple in mots:
+        mot = ''.join(mot_tuple)
+        print(len(mot), mot)
+        for char in mot:
+            wtotal += 1
+            query = "UPDATE LETTRES SET nb_occurence = nb_occurence + 1 WHERE lettre = '" + char + "'"
+            curseur.execute(query)
+    if wtotal > 0:
+        query = "UPDATE LETTRES SET taux = nb_occurence / " + str(wtotal)
+        curseur.execute(query)
+    conn.commit()
+
+def difficulte(langue):
+    """
+    Calcule la difficulté des mots pour une langue donnée
+    et met à jour la colonne `difficulte` de la table MOTS.
+    """
+    query = "UPDATE MOTS SET difficulte = 0 WHERE langue = '" + langue + "'"
+    curseur.execute(query)
+    conn.commit()
+
+    query = "SELECT mot FROM MOTS WHERE langue = '" + langue + "'"
+    curseur.execute(query)
+    mots = curseur.fetchall()
+
+    for mot_tuple in mots:
+        mot = ''.join(mot_tuple)
+        print(len(mot), mot)
+        note = 0
+        query = "SELECT taux FROM LETTRES WHERE lettre = '" + mot[0] + "'"
+        curseur.execute(query)
+        taux_debut = curseur.fetchone()
+
+        if taux_debut:
+            note += 1 / taux_debut[0]
+
+        for i in range(1, len(mot)):
+            char = mot[i]
+            submot = mot[1:i-1]
+            if char not in "- .":
+                if char not in submot:
+                    query = "SELECT taux FROM LETTRES WHERE lettre = '" + char + "'"
+                    curseur.execute(query)
+                    taux_char = curseur.fetchone()
+                    if taux_char:
+                        note += 1 / taux_char[0]
+
+        if len(mot) > 4:
+            note /= len(mot)
+
+        query = "UPDATE MOTS SET difficulte = " + str(note) + " WHERE mot = '" + mot + "' AND langue = '" + langue + "'"
+        curseur.execute(query)
+    conn.commit()
 
 conn = mariadb.connect(
     user="project",
@@ -21,100 +116,25 @@ conn = mariadb.connect(
     host="109.10.114.61",
     port=3306,
     database="PenduNSI"
-
 )
-
 curseur = conn.cursor()
+query = "SELECT code FROM LANGUE "
+curseur.execute(query)
+langues=curseur.fetchall()
+print(langues)
+wlangues=[]
+wlangues.append(langues)
 
 
-def remplir_mots():
-    i=1
-    curseur.execute("DELETE FROM MOTS")
-    conn.commit()
-    with open('Liste_mots_nsi.csv', 'r') as file:
-        reader = csv.reader(file, delimiter=';')
-        for row2 in reader:
-            row=unidecode.unidecode(''.join(row2).upper().strip())
-            print(i,row)
-            x=row.find("'")
-            if x == -1 :
-                wrequete="INSERT INTO MOTS VALUES ('"+row+"','0')"
-                curseur.execute(wrequete)
-            i=i+1
-    conn.commit()
-         
-            
-        
-        
-def calcul_proba_lettres():
-    wtotal=0       
-    curseur.execute("DELETE FROM LETTRES")
-    conn.commit()
-    mettre_lettre()
-    wrequete = ("SELECT mot FROM MOTS")
-    curseur.execute(wrequete)
-    wresultat=curseur.fetchall()
-    for wmot2 in wresultat:
-        wmot=''.join(wmot2)
-        wlongueur=len(wmot)
-        print(wlongueur,wmot)
-        for i in range(wlongueur):
-            wtotal=wtotal+1
-            wcar=wmot[i]
-            wrequete = ("UPDATE LETTRES SET nb_occurence=nb_occurence+1 WHERE lettre='"+wcar+"';")
-            curseur.execute(wrequete)
-    wrequete="UPDATE LETTRES SET taux = (nb_occurence/"+str(wtotal)+")"
-    curseur.execute(wrequete)
-    conn.commit()
-def difficulte():
-        
-    wrequete = ("UPDATE MOTS SET difficulte=0")
-    curseur.execute(wrequete) 
-    conn.commit()    
-    wrequete = ("SELECT mot FROM MOTS")
-    curseur.execute(wrequete)
-    wresultat=curseur.fetchall()
-    for wmot2 in wresultat:
-        wmot=''.join(wmot2)
-        wlongueur=len(wmot)
-        print(wlongueur,wmot)
-        wnote=0
-        wcar=wmot[0]
-        wrequete = ("SELECT taux FROM LETTRES WHERE LETTRE = '"+wcar+"'")
-        curseur.execute(wrequete)
-        wstrtaux=curseur.fetchone()
-        for wtaux in wstrtaux:
-            wnote=wnote+1/wtaux
-        for i in range(1,len(wmot)):
-            wcar=wmot[i]
-            wpartiemot=wmot[1:i-1]
-            if wcar !='-' and wcar != ' ' and wcar != '.':
-                if wpartiemot.find(wcar) == -1 :
-                    wrequete = ("SELECT taux FROM LETTRES WHERE LETTRE = '"+wcar+"'")
-                    curseur.execute(wrequete)
-                    wstrtaux=curseur.fetchone()
-        
-                    for wtaux in wstrtaux :
-                        #print(wtaux)
-                        wnote=wnote+1/wtaux
-        if len(wmot)>4:            
-            wnote=wnote/len(wmot)
-        
-        wrequete2 = "UPDATE MOTS SET difficulte = '"+str(wnote)+"' WHERE mot = '"+wmot+"'"
-        #print(wrequete2)
-        curseur.execute(wrequete2)
-        conn.commit()
-    
-        
-    
-mettre_lettre()
-remplir_mots()
-calcul_proba_lettres()
-difficulte()
-
-    
-    
-
-
+for langue in wlangues:
+    code = langue[0]
+    print(code)
+    remplir_mots(code)
+    calcul_proba_lettres(code)
+    difficulte(code)
+    query = "SELECT * FROM LETTRES"
+    curseur.execute(query)
+    test = curseur.fetchall()
+    print(test)
 
 conn.close()
--- pendu.py
+++ pendu.py
@@ -1,140 +1,205 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import *
 import requests
 import random
+from tkinter import messagebox
 from PIL import Image, ImageTk
 import mariadb
+import io  # Pour lire les images en mémoire
 
+# Adresse du serveur de base de données
+wserveur = "109.10.114.61"
 
+# Connexion à la base de données
 conn = mariadb.connect(
     user="project",
     password="pm",
-    host="109.10.114.61",
+    host=wserveur,
     port=3306,
     database="PenduNSI"
-
 )
 
 curseur = conn.cursor()
 
-
-
-def creation_liste(wdif1,wdif2) :
-    wrequete="SELECT mot FROM MOTS WHERE difficulte<="+str(wdif2)+" AND difficulte>="+str(wdif1)
-    curseur.execute(wrequete)
-    wliste=curseur.fetchall()
+def creation_liste(wdif1, wdif2, langue):
+    query = "SELECT mot FROM MOTS WHERE difficulte <= " + str(wdif2) + " AND difficulte >= " + str(wdif1) + " AND langue = '" + langue + "'"
+    curseur.execute(query)
+    wliste = curseur.fetchall()
     return wliste
-        
+
 def choix_mot(wliste):
     wmot = random.choice(wliste)
-    wmot=''.join(wmot)
+    wmot = ''.join(wmot)
     return str(wmot)
 
-def mot_en_liste(mot):
-    return ['_' for _ in mot]
+def cacher_mot(mot):
+    return ['*' for _ in mot]
 
-def precharger_images():
-    images = []
-    for i in range(11):
-        image_url = f"http://109.10.114.61/{i}.png"
-        response = requests.get(image_url, stream=True)
-        if response.status_code == 200:
-            image_data = Image.open(response.raw)
-            image_resized = image_data.resize((200, 200))
-            images.append(ImageTk.PhotoImage(image_resized))
-        else:
-            images.append(None)
-    return images
+def dessiner_pendu(zone_dessin, nb_tentatives, wratio):
+    if nb_tentatives == 1:
+        zone_dessin.create_line(150 * wratio, 490 * wratio, 250 * wratio, 490 * wratio, width=8 * wratio, fill="black")
+    elif nb_tentatives == 2:
+        zone_dessin.create_line(200 * wratio, 490 * wratio, 200 * wratio, 20 * wratio, width=8 * wratio, fill="black")
+    elif nb_tentatives == 3:
+        zone_dessin.create_line(196.5 * wratio, 20 * wratio, 404 * wratio, 20 * wratio, width=8 * wratio, fill="black")
+    elif nb_tentatives == 4:
+        zone_dessin.create_line(400 * wratio, 20 * wratio, 400 * wratio, 50 * wratio, width=8 * wratio, fill="black")
+    elif nb_tentatives == 5:
+        zone_dessin.create_oval(375 * wratio, 50 * wratio, 425 * wratio, 100 * wratio, width=8 * wratio, outline="black")
+    elif nb_tentatives == 6:
+        zone_dessin.create_line(400 * wratio, 100 * wratio, 400 * wratio, 200 * wratio, width=8 * wratio, fill="black")
+    elif nb_tentatives == 7:
+        zone_dessin.create_line(400 * wratio, 120 * wratio, 360 * wratio, 170 * wratio, width=8 * wratio, fill="black")
+    elif nb_tentatives == 8:
+        zone_dessin.create_line(400 * wratio, 120 * wratio, 440 * wratio, 170 * wratio, width=8 * wratio, fill="black")
+    elif nb_tentatives == 9:
+        zone_dessin.create_line(400 * wratio, 200 * wratio, 360 * wratio, 280 * wratio, width=8 * wratio, fill="black")
+    elif nb_tentatives == 10:
+        zone_dessin.create_line(400 * wratio, 200 * wratio, 440 * wratio, 280 * wratio, width=8 * wratio, fill="black")
 
-def ouvrir_jeu(borne_mini,borne_maxi):
+def changer_langue(langue):
+    global wlangue
+    wlangue = langue
+    query = "SELECT libelle,mess FROM LANGUE WHERE code='" + wlangue + "'"
+    curseur.execute(query)
+    message = curseur.fetchone()
+    messagebox.showinfo("", "Vous avez choisi " + message[0] + "\n" + message[1])
+    query = str("SELECT MAX(difficulte) FROM MOTS WHERE langue='"+wlangue+"'")
+    curseur.execute(query)
+    wdiff=curseur.fetchone()
+    global wdiffmax
+    wdiffmax=float(wdiff[0])
+    print(wdiffmax)
+
+
+def ajout_menu(param_langue):
+    submenu.add_command(image=tk_image[i], command=lambda: changer_langue(param_langue))
+
+def ouvrir_jeu(borne_mini, borne_maxi, wlangue):
     fenetre_jeu = tk.Toplevel(fenetre_principale)
+    wlargeur = fenetre_jeu.winfo_screenwidth()
+    whauteur = fenetre_jeu.winfo_screenheight()
+    wminimum = min(wlargeur, whauteur)
+    wratio = wminimum / 700
+    wtaille = str(wminimum) + "x" + str(wminimum)
     fenetre_jeu.title('Jeu du Pendu')
-    fenetre_jeu.geometry("600x600+200+50")
+    fenetre_jeu.geometry(wtaille)
     fenetre_jeu.resizable(height=True, width=True)
     fenetre_jeu.configure(bg="white")
 
-    frame_listbox = tk.Frame(fenetre_jeu)
-    frame_listbox.pack(fill=tk.BOTH, expand=True)
+    bg_image_path = "0.jpg"
+    bg_image = Image.open(bg_image_path)
+    bg_image = bg_image.resize((int(500 * wratio), int(500 * wratio)))
+    bg_image_tk = ImageTk.PhotoImage(bg_image)
 
-    listbox = tk.Listbox(fenetre_jeu, font=("Arial", 16), selectbackground="#4286f4", selectforeground="white")
-    listbox.pack(pady=10, padx=10, fill=tk.BOTH, expand=True)
+    zone_dessin = Canvas(fenetre_jeu, width=500 * wratio, height=500 * wratio, bg="white", bd=0)
+    zone_dessin.pack()
+    zone_dessin.create_image(0, 0, anchor="nw", image=bg_image_tk)
+    zone_dessin.image = bg_image_tk
+
+    label_mot = tk.Label(fenetre_jeu, text="", font=("Arial", int(24 * wratio)), bg="white")
+    label_mot.pack(pady=int(10 * wratio), padx=int(10 * wratio))
 
     frame_boutons = tk.Frame(fenetre_jeu)
-    frame_boutons.pack(pady=5)
+    frame_boutons.pack(pady=int(5 * wratio))
 
-    frame_image = tk.Frame(fenetre_jeu)
-    frame_image.pack(pady=10)
-    label_image = tk.Label(frame_image)
-    label_image.pack()
-    wl1=creation_liste(borne_mini, borne_maxi)
+    wl1 = creation_liste(borne_mini, borne_maxi, wlangue)
     mot = choix_mot(wl1)
-    liste_underscores = mot_en_liste(mot)
+    liste_sous_tirets = cacher_mot(mot)
     nb_tentatives = 0
     max_tentatives = 10
     fenetre_principale.withdraw()
 
-    images_pendu = precharger_images()
-
     def mettre_a_jour_affichage():
-        underscore_line = " ".join(liste_underscores)
-        listbox.delete(0, tk.END)
-        listbox.insert(tk.END, underscore_line)
+        underscore_line = " ".join(liste_sous_tirets)
+        label_mot.config(text=underscore_line)
 
-    def mettre_a_jour_image(nb_tentatives):
-        if 0 <= nb_tentatives < len(images_pendu):
-            label_image.config(image=images_pendu[nb_tentatives])
-            label_image.image = images_pendu[nb_tentatives]
-
-    def bouton_click(letter, button):
+    def saisir_lettre(lettre, bouton):
         nonlocal nb_tentatives
-        letter = letter.upper()
-        if letter in mot:
+        lettre = lettre.upper()
+        if lettre in mot:
             for i in range(len(mot)):
-                if mot[i] == letter:
-                    liste_underscores[i] = letter
+                if mot[i] == lettre:
+                    liste_sous_tirets[i] = lettre
             mettre_a_jour_affichage()
-            if "_" not in liste_underscores:
+            if "*" not in liste_sous_tirets:
                 messagebox.showinfo("Félicitations", "Vous avez gagné !")
                 fenetre_principale.deiconify()
         else:
             nb_tentatives += 1
-            mettre_a_jour_image(nb_tentatives)
+            dessiner_pendu(zone_dessin, nb_tentatives, wratio)
             if nb_tentatives >= max_tentatives:
-                messagebox.showinfo("Défaite", "Vous avez perdu !"+mot)
-                
+                messagebox.showinfo("Défaite", "Le mot était : " + mot)
                 fenetre_principale.deiconify()
-        button.config(state="disabled")
+
+        bouton.config(state="disabled")
 
     mettre_a_jour_affichage()
-    mettre_a_jour_image(nb_tentatives)
 
-    lettres = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-               "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
+    lettres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for index, lettre in enumerate(lettres):
-        row = index // 13
-        col = index % 13
-        bouton = tk.Button(frame_boutons, text=lettre, font=("Tahoma", 12))
-        bouton.config(command=lambda l=lettre, b=bouton: bouton_click(l, b))
+        row = int(index // 13)
+        col = int(index % 13)
+        bouton = tk.Button(frame_boutons, text=lettre, font=("Tahoma", int(12 * wratio)))
+        bouton.config(command=lambda l=lettre, b=bouton: saisir_lettre(l, b))
         bouton.grid(row=row, column=col, padx=5, pady=5)
-creation_liste(1000,1200)
-fenetre_principale = tk.Tk()
-fenetre_principale.title("Choix difficulté")
-fenetre_principale.geometry("400x300")
 
-label_principal = tk.Label(fenetre_principale, text="Quelle difficulté voulez-vous ?", font=("Arial", 14))
+# Télécharger les images des drapeaux
+def telecharger_image(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return Image.open(io.BytesIO(response.content))
+
+
+fenetre_principale = tk.Tk()
+fenetre_principale.title("Choix difficulté et langue")
+fenetre_principale.geometry("400x400")
+
+label_principal = tk.Label(fenetre_principale, text="Quelle difficulté et langue voulez-vous ?", font=("Arial", 14))
 label_principal.pack(pady=20)
 
-bouton_facile = tk.Button(fenetre_principale, text="Facile", font=("Arial", 12), command=lambda: ouvrir_jeu(0,50))
+wlangue = "fr"
+
+menu_bar = Menu(fenetre_principale)
+submenu = Menu(menu_bar, tearoff=0)
+
+query = "SELECT * FROM LANGUE"
+curseur.execute(query)
+j = curseur.fetchall()
+tk_image = []
+pil_image = []
+wnomvar = []
+wchaine = []
+
+for i in range(len(j)):
+    url_drapeau = str("http://109.10.114.61/PenduNSI/flag_"+j[i][0]+".jpg")
+    image = telecharger_image(url_drapeau)
+    if image:
+        pil_image.append(image)
+        tk_image.append(ImageTk.PhotoImage(image))
+        wchaine.append(j[i][0])
+        ajout_menu(j[i][0])
+
+menu_bar.add_cascade(label="Menu", menu=submenu)
+fenetre_principale.config(menu=menu_bar)
+
+query = str("SELECT MAX(difficulte) FROM MOTS WHERE langue='"+wlangue+"'")
+curseur.execute(query)
+wdiff=curseur.fetchone()
+wdiffmax=float(wdiff[0])
+print(wdiffmax)
+
+
+bouton_facile = tk.Button(fenetre_principale, text="Facile", font=("Arial", 12), command=lambda: ouvrir_jeu(0, wdiffmax/6, wlangue))
 bouton_facile.pack(pady=10)
 
-bouton_normal = tk.Button(fenetre_principale, text="Normal", font=("Arial", 12), command=lambda: ouvrir_jeu(51,250))
+bouton_normal = tk.Button(fenetre_principale, text="Normal", font=("Arial", 12), command=lambda: ouvrir_jeu(wdiffmax/6, wdiffmax/2, wlangue))
 bouton_normal.pack(pady=10)
 
-bouton_difficile = tk.Button(fenetre_principale, text="Difficile", font=("Arial", 12), command=lambda: ouvrir_jeu(251,750))
+bouton_difficile = tk.Button(fenetre_principale, text="Difficile", font=("Arial", 12), command=lambda: ouvrir_jeu(wdiffmax/2,wdiffmax*(5/6), wlangue))
 bouton_difficile.pack(pady=10)
 
-bouton_extreme = tk.Button(fenetre_principale, text="Extrême", font=("Arial", 12), command=lambda: ouvrir_jeu(751,10000))
+bouton_extreme = tk.Button(fenetre_principale, text="Extrême", font=("Arial", 12), command=lambda: ouvrir_jeu(wdiffmax*0.833, round(wdiffmax,0), wlangue))
 bouton_extreme.pack(pady=10)
 
 fenetre_principale.mainloop()
